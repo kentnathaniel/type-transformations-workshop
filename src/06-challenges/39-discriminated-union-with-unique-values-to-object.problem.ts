ae8c1849fb562936cb35d11db007bcca27eedf47
kentnathaniel <kent.nathaniel2@gmail.com>
2024-04-30 05:28:51 +0700

    feat: initial commit

--- src/06-challenges/39-discriminated-union-with-unique-values-to-object.problem.ts
+++ src/06-challenges/39-discriminated-union-with-unique-values-to-object.problem.ts
@@ -1,32 +1,41 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils'
 
 type Route =
   | {
-      route: "/";
+      route: '/'
       search: {
-        page: string;
-        perPage: string;
-      };
+        page: string
+        perPage: string
+      }
     }
-  | { route: "/about" }
-  | { route: "/admin" }
-  | { route: "/admin/users" };
+  | { route: '/about' }
+  | { route: '/admin' }
+  | { route: '/admin/users' }
 
-type RoutesObject = unknown;
+// This solution will return this error
+// Type '"search"' cannot be used to index type 'K'
+// I guess it is because the the TS compiler can't detect whether "search" key will exist in the K until runtime
+type RoutesObject2 = {
+  [K in Route as K['route']]: K['search'] extends object ? K['search'] : never
+}
+
+type RoutesObject = {
+  [K in Route as K['route']]: 'search' extends keyof K ? K['search'] : never
+}
 
 type tests = [
   Expect<
     Equal<
       RoutesObject,
       {
-        "/": {
-          page: string;
-          perPage: string;
-        };
-        "/about": never;
-        "/admin": never;
-        "/admin/users": never;
+        '/': {
+          page: string
+          perPage: string
+        }
+        '/about': never
+        '/admin': never
+        '/admin/users': never
       }
     >
-  >,
-];
+  >
+]
